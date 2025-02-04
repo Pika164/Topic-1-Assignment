b3f8a61980f7db83a9a5d32199a115326259af02
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-04 14:45:32 -0500

    Finished.

--- Topic 1 Assignment/Program.cs
+++ Topic 1 Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Linkin Krysa
+            Console.WriteLine("Hello! My name is Linkin Krysa");
+            Console.WriteLine("");
+            Console.WriteLine("Comments are helpful so people can explain their code to the person or people that are viewing the code that you made or explain why they did certain things.");
+            Console.WriteLine("");
+            Console.WriteLine("L           K    K");
+            Console.WriteLine("L           K   K");
+            Console.WriteLine("L           K  K");
+            Console.WriteLine("L           K K");
+            Console.WriteLine("L           K  K");
+            Console.WriteLine("L           K   K");
+            Console.WriteLine("L L L L L   K    K");
         }
     }
 }
